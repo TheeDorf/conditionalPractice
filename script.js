9d4b6c4d45a35820625356a7d42c3f10fe7bcb76
AJ Dorfman <ajdorfman91@gmail.com>
2023-03-13 08:19:57 -0700

    selector practice

---
+++ script.js
@@ -0,0 +1,29 @@
+// let str1 ="lasagna"
+// console.log()
+
+// let arr1 = [1,2,3,4,5, "its a me, the poser mario"]
+// console.log(arr1[5])
+
+
+
+// //  if(str1.substring(1)){
+//     // console.log("buon appetito")}
+// // else{
+//     // console.log("whats a matta you")}
+
+
+let path = "bad"
+
+switch (path) {
+    case "good":
+        console.log("you have made a good choice")
+        
+        break;
+    case "bad":
+        console.log("bad, wrong!")
+        break;
+
+    default:
+        console.log("if you choose not to decide you still have made a choice")
+        break;
+}
